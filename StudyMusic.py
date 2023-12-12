cf5e09d0115e110b8c5d3d2de02245bff8dc8fd9
wndrsn1 <146864301+wndrsn1@users.noreply.github.com>
2023-12-12 10:00:50 -0600

    Update StudyMusic.py

--- StudyMusic.py
+++ StudyMusic.py
@@ -1,15 +1,40 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import time
-from config import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI
+from config import CLIENT_ID,CLIENT_SECRET,REDIRECT_URI
 
+# Scope determines the level of access your app will have
+SCOPE = 'playlist-read-private user-library-read user-read-playback-state user-modify-playback-state'
 
-SCOPE = 'user-read-playback-state user-modify-playback-state'
-
+# Set up Spotify API connection
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
                                                client_secret=CLIENT_SECRET,
                                                redirect_uri=REDIRECT_URI,
                                                scope=SCOPE))
+
+# Retrieve all playlists and their URIs
+def get_playlists():
+    playlists = sp.current_user_playlists()
+    for playlist in playlists['items']:
+        playlist_name = playlist['name']
+        playlist_uri = playlist['uri']
+        print(f"Playlist Name: {playlist_name}\nPlaylist URI: {playlist_uri}\n")
+
+# Prompt the user to enter the name of the playlist
+def get_playlist_uri_by_name():
+    playlist_name_to_find = input("Enter the name of the playlist you would like to play: ")
+    
+    # Retrieve all playlists
+    playlists = sp.current_user_playlists()
+
+    # Search for the playlist with the specified name
+    for playlist in playlists['items']:
+        if playlist['name'] == playlist_name_to_find:
+            playlist_name = playlist['name']
+            playlist_uri = playlist['uri']
+            return playlist_uri
+    
+    print(f"Playlist not found '{playlist_name_to_find}' .")
 
 
 def play_playlist(playlist_uri):
@@ -26,11 +51,14 @@ def pause_playback():
 def disconnect():
     sp.auth_manager.get_access_token(as_dict=False)
 
+# Example usage
+get_playlists()
+timer = float(input('How long would you like to study?(mins) '))
+if isinstance(timer, float):
+    playlist_uri = get_playlist_uri_by_name()
+    play_playlist(playlist_uri)
+    time.sleep(timer*60)  
+    pause_playback()
+    disconnect()
 
-
-playlist_uri = 'spotify:playlist:0vvXsWCC9xrXsKd4FyS8kM'
-play_playlist(playlist_uri)
-timer = int(input('How long would you like to study?(min) '))
-time.sleep(timer*60)  
-pause_playback()
-disconnect()
+else: print('Please enter an integer!')
